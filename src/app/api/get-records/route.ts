9eb367f6442067e6a880be944c9afc08fddaac68
Devansh Prajapati <98266228+Devansh1010@users.noreply.github.com>
2025-07-05 22:00:03 +0530

    simple add console.log

--- src/app/api/get-records/route.ts
+++ src/app/api/get-records/route.ts
@@ -1,5 +1,7 @@
 import { medicalRecord } from "@/medical-records"
 export async function GET(req: Request) {
+
+    console.log(medicalRecord)
     return Response.json({
         medicalRecord
     })
