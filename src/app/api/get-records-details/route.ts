c979576b272065da77331313885cbd1df9c7ffa0
Devansh Prajapati <98266228+Devansh1010@users.noreply.github.com>
2025-07-09 23:48:36 +0530

    add route to get names and ids of patient

---
+++ src/app/api/get-records-details/route.ts
@@ -0,0 +1,41 @@
+import { dbConnect } from "@/lib/dbConnect"
+import Patient from "@/models/Patient"
+import { patchFetch } from "next/dist/server/app-render/entry-base";
+
+
+export async function GET(req: Request) {
+
+    await dbConnect()
+
+    try {
+
+        const patients = await Patient.find()
+
+        const names = patients.map((p) => p.name)
+        const ids = patients.map((p) => p._id)
+
+        if (!patients) {
+            return Response.json({
+                success: true,
+                message: "Patients not found",
+                names: [],
+                ids: []
+            }, { status: 400 });
+        }
+
+        return Response.json({
+            success: true,
+            message: "Patient record saved successfully",
+            names,
+            ids
+        }, { status: 201 });
+
+    } catch (error) {
+
+        return Response.json({
+            success: true,
+            message: "Error getting patients"
+        }, { status: 500 });
+
+    }
+}
